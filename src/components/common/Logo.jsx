610a116d5d841ef346260a7ea74ed797c99d4a48
zzitheright <zzitheright@gmail.com>
2024-04-07 17:19:18 +0900

    240407 수업 종료

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 function Logo() {
-    return <h1 className="text-3xl font-bold"><Link to='/'>logo</Link></h1>
+    return <h1 className="text-3xl font-bold text-secondary"><Link to='/'>logo</Link></h1>
 }
 
 export default Logo;
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,18 @@
 import { Link, useLocation } from "react-router-dom";
 import Logo from "../common/Logo";
 import Button, { GhostButton } from "../button/Button";
+import tw from "twin.macro";
+import styled from "styled-components";
 
 function Header() {
   const location = useLocation();
 
   return(
-    <header className="flex justify-between items-center bg-gray-100 p-5 border-b-2 border-indigo-500">
+    <header className="flex justify-between items-center bg-gray-100 p-5 bg-primary border-b-2 border-indigo-500">
       {/* <h1>logo</h1> */}
       <Logo />
-      <nav>
-        <ul className="flex gap-6">
+      <Gnb>
+        <ul>
           <li className={location.pathname === '/' ? 'active' : ''}>
             <Link to="/">Home</Link>
           </li>
@@ -21,7 +23,7 @@ function Header() {
             <Link to="/login">Login</Link>
           </li>
         </ul>
-      </nav>
+      </Gnb>
 
       <div>
         <Button default>마이페이지</Button>
@@ -32,4 +34,15 @@ function Header() {
   )
 }
 
+// const Gnb = tw.nav`
+//   bg-secondary p-3
+// `
+
+const Gnb = styled.nav`
+  ul {
+    ${tw`flex gap-6`}
+    li {
+    }
+  }
+`
 export default Header;
